8983b1607e534ca8882fa67feb8206ee2f6fbb1a
auny silvia <aunisilvia@gmail.com>
2025-12-15 11:25:39 +0700

    day 1

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+ route::get('/tentang', function (){
+    return view('tentang');
+ });
+ route::get('/sapa/{nama}', function ($nama){
+    return "hallo,$nama! Selamat datang di toko gehel.";
+ });
+ route::get('/kategori/{nama?}', function ($nama = 'Semua'){
+    return "Menampilkan kategori: $nama";
+ });
+ Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
