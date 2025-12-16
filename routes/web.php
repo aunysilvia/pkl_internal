fef73eedd631970abfaffd9dd766dea65912e3d1
auny silvia <aunisilvia@gmail.com>
2025-12-16 19:59:36 +0700

    day2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
  Route::get('/produk/{id}', function ($id) {
     return "Detail produk #$id";
 })->name('produk.detail');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
